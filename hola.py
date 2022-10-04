62f53dd98945defd35d7f590e9584d78217919a4
Ruben <ruben.dr85@gmail.com>
2022-10-04 14:41:38 +0200

    asignacion de variables

--- hola.py
+++ hola.py
@@ -2,5 +2,5 @@ import os
 print("hello world desde España")
 
 # read enviroment variables from file
-print(os.environ['PATH'])
-print(os.environ['HOME'])
+path = os.environ['PATH']
+home = os.environ['HOME']

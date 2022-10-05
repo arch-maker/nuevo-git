af4fd5de5da670969ecbaabb85463298f3a73e90
Ruben <ruben.dr85@gmail.com>
2022-10-05 10:42:16 +0200

    Cambios en la nueva Rama

--- hola.py
+++ hola.py
@@ -2,6 +2,7 @@ import os
 print("hello world desde España")
 
 # read enviroment variables from file
+# Asignacion de paths a variables
 path = os.environ['PATH']
 home = os.environ['HOME']
 # fin del programa
